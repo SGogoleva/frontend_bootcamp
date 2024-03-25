0f72875303697629a77d87c395da18dade6fde4a
Sofia <isofa2012@gmail.com>
2024-03-25 19:13:40 +0200

    Click function added for inventory, to be completed further

--- projects/Minecraft_project/main.js
+++ projects/Minecraft_project/main.js
@@ -1,9 +1,9 @@
 const playField = document.getElementsByClassName("tile")
-// let b = playField.querySelectorAll("div")
-console.log(playField.length)
+
 const getRandomInt = function(num) {
     return Math.floor(Math.random() * num) + 1
 }
+
 const colorTiles = function() {
     for(let i = 0; i < playField.length; i++) {
         const bgColorId = getRandomInt(3)
@@ -33,10 +33,57 @@ const getGrassTiles = function() {
         }
     }
 }
-
 getGrassTiles()
 
+const axeField = document.getElementById("axe")
+const pickaxeField = document.getElementById("pickaxe")
+const shovelField = document.getElementById("shovel")
+const makeFieldActive = function(field) {
+    field.style.border = "2px solid #371AB8"
+}
 
+// axeField.addEventListener("click", () => {
+//     if (this.style.border === "1px solid #F3F3F3") {
+//         this.style.border = "2px solid #371AB8"
+//     }
+//     else {
+//         this.style.border = "1px solid #F3F3F3"
+//     }
+// })
+
+
+
+
+// axeField.addEventListener("click", makeFieldActive(axeField), false)
+
+// makeFieldActive(axeField)
+const toolList = document.getElementsByClassName("tool")
+const tryY = window.getComputedStyle(axeField).getPropertyValue("border")
+console.log(tryY)
+
+for(let i = 0; i < toolList.length; i++) {
+    let currentStatus = window.getComputedStyle(toolList[i]).getPropertyValue("border")
+    toolList[i].addEventListener("click", () => {
+        if (currentStatus === "0.8px solid rgb(243, 243, 243)") {
+            toolList[i].style.border = "2px solid #371AB8"
+            currentStatus = window.getComputedStyle(toolList[i]).getPropertyValue("border")
+        }
+        else {
+            toolList[i].style.border = "1px solid #F3F3F3"
+            }
+        })
+    console.log(currentStatus)
+}
+
+// for(let i = 0; i < toolList.length; i++) {
+//     toolList[i].addEventListener("click", () => {
+//         toolList[i].style.border = "2px solid #371AB8"
+//     })
+// }
+// 0.8px solid rgb(243, 243, 243)
+//1px solid #F3F3F3
+
+// console.log(toolList)
 
 // const bgColorId = function (max = 3) {
 //     return Math.floor(Math.random() * max) + 1;
@@ -44,3 +91,4 @@ getGrassTiles()
 // console.log(bgColorId())
 
 // rgb(71, 118, 57) for grass
+
